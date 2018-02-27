0f24d886daa1732552b134c374e930c439f44d01
Pedro Saleiro <pedrosaleiro@gmail.com>
2018-02-27 14:49:27 -0600

    bias_min_group_metric fixed

--- src/aequitas/bias.py
+++ src/aequitas/bias.py
@@ -1,3 +1,6 @@
+import logging
+from sys import exit
+
 import numpy as np
 import pandas as pd
 
@@ -18,20 +21,30 @@ class Bias(object):
         we want to calculate the Disparity values
         :return: a dataframe
         """
+
+        print('get_bias_min_metric')
         fill_1 = {}
         key_cols = ['model_id', 'parameter', 'group_variable']
-        attr_groupby = df.groupby(key_cols)
         for group_metric in self.group_metrics:
-            fill_1[group_metric] = 1.000000
+            fill_1[group_metric + ' Disparity'] = 1.000000
             # creating disparity by dividing each group metric value by the corresponding min
             # value from the groups of the target attribute
-            df[group_metric + ' Disparity'] = df[group_metric] / attr_groupby[
-                group_metric].transform(min)
             df2 = pd.DataFrame()
-            # but we also want to get the group_value of the reference group for each bias metric
-            df2[key_cols + [group_metric + '_ref_group_value']] = df.loc[attr_groupby[
-                group_metric].idxmin()][key_cols + ['group_value']]
+            try:
+                # this groupby is being called every cycle. maybe we can create a list of df_groups
+                # and merge df at the end? it can not be simply put outside the loop(the merge...)
+                df_group = df.loc[df.groupby(key_cols)[group_metric].idxmin()]
+                # but we also want to get the group_value of the reference group for each bias metric
+                df2[key_cols + [group_metric + ' Disparity', group_metric + '_ref_group_value']] = \
+                    df_group[key_cols + [group_metric, 'group_value']]
+            except KeyError:
+                logging.error(
+                    'get_bias_min_metric:: one of the following columns is not on the input '
+                    'dataframe : model_id ,parameter,group_variable or any of the group_metrics '
+                    'list')
+                exit()
             df = df.merge(df2, on=key_cols)
+            df[group_metric + ' Disparity'] = df[group_metric] / df[group_metric + ' Disparity']
         # We are capping the disparity values to 10.0 when divided by zero...
         df = df.replace(np.inf, self.fill_divbyzero)
         df = df.fillna(value=fill_1)
@@ -39,19 +52,27 @@ class Bias(object):
 
     def get_bias_major_group(self, df):
         """
+            Calculates the bias (disparity) metrics for the predefined list of group metrics
+            using the majority group within each attribute as the reference group (denominator)
 
-        :param df:
-        :return:
+        :param df: the returning dataframe from the group.get_crosstabs
+        :return: a dataframe with the bias metrics as new columns and the ref group,
+                it must have the same number of rows as the input dataframe
         """
         print('get_bias_major_group')
         fill_1 = {}
         key_cols = ['model_id', 'parameter', 'group_variable']
-        df_group = df.loc[df.groupby(key_cols)['group_size'].idxmax()]
+        try:
+            df_group = df.loc[df.groupby(key_cols)['group_size'].idxmax()]
+        except KeyError:
+            logging.error('get_bias_major_group:: one of the following columns is not on the input '
+                          'dataframe : model_id ,parameter,group_variable, group_size ')
+            exit()
         count = 0
         for group_metric in self.group_metrics:
             count += 1
             print(count, group_metric)
-            fill_1[group_metric] = 1.000000
+            fill_1[group_metric + ' Disparity'] = 1.000000
             df2 = pd.DataFrame()
             # but we also want to get the group_value of the reference group for each bias metric
             # here we just getting the ref group metric value in the disparity column (it is not
@@ -66,7 +87,7 @@ class Bias(object):
         df = df.fillna(value=fill_1)
         return df
 
-    def get_bias_predefined_group(self, df, ref_groups_list):
+    def get_bias_predefined_group(self, df, ref_groups_dict):
         """
 
         :param df:
@@ -74,4 +95,6 @@ class Bias(object):
         :param ref_groups_list:
         :return:
         """
+
+        #ref_groups_dict = {'race':'white','age':'18-25'}
         return df
