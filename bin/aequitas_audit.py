849743f0fdedfb0143c7667119c2aa0fb6bcd4d6
Pedro Saleiro <pedrosaleiro@gmail.com>
2018-03-02 17:42:11 -0600

    creating report

--- bin/aequitas_audit.py
+++ bin/aequitas_audit.py
@@ -1,4 +1,3 @@
-
 import argparse
 import logging
 from sys import exit
@@ -6,32 +5,36 @@ from sys import exit
 import pandas as pd
 import yaml
 
-from bin.utils.dsapp import create_bias_tables
-from bin.utils.dsapp import get_dsapp_data
-from bin.utils.dsapp import get_engine
-from bin.utils.dsapp import get_models
+from bin.utils.db import create_bias_tables
+from bin.utils.db import get_dsapp_data
+from bin.utils.db import get_engine
+from bin.utils.db import get_models
+from bin.utils.pdf_creator import PDF
 from src.aequitas.bias import Bias
+from src.aequitas.fairness import Fairness
 from src.aequitas.group import Group
 
 about = """
-##########################################################################
-##   Center for Data Science and Public Policy                          ##
-##   http://dsapp.uchicago.edu                                          ##
-##                                                                      ##
-##   Copyright 2017. The University of Chicago. All Rights Reserved.    ## 
-##########################################################################
+############################################################################
+##   Center for Data Science and Public Policy                            ##
+##   http://dsapp.uchicago.edu                                            ##
+##                                                                        ##
+##   Copyright \xa9 2017. The University of Chicago. All Rights Reserved.    ## 
+############################################################################
 
 
 
-_________________________________________________________________________
+____________________________________________________________________________
                                      _ _            
                __ _  ___  __ _ _   _(_) |_ __ _ ___ 
               / _` |/ _ \/ _` | | | | | __/ _` / __| 
              | (_| |  __/ (_| | |_| | | || (_| \__ \ 
               \__,_|\___|\__, |\__,_|_|\__\__,_|___/
                             |_|                            
-_________________________________________________________________________  
 
+____________________________________________________________________________  
+
+                    Bias and Fairness Analysis
 
 
 """
@@ -90,7 +93,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def run_dsapp(engine, configs):
+def run_db(engine, configs):
     """
 
     :param engine:
@@ -125,8 +128,7 @@ def run_dsapp(engine, configs):
     b = Bias()
     bias_df = b.get_disparity_major_group(group_results)
     print('number of rows after bias majority ref group:', len(bias_df))
-    # bias_df = b.get_disparity_min_metric(group_results)
-    # print(bias_df.columns.values)
+    print('Any NaN?: ', bias_df.isnull().values.any())
     """
     if 'reference_groups' in configs:
         bias_df = b.get_disparity_predefined_groups(group_results, configs['reference_groups'])
@@ -134,15 +136,47 @@ def run_dsapp(engine, configs):
     """
 
     print('df shape after bias minimum per metric ref group:', bias_df.shape)
-    print(bias_df[['group_variable', 'group_value', 'parameter', 'FDR', 'FDR_disparity',
-                   'FDR_ref_group_value']])
-    print('Any NaN?: ', bias_df.isnull().values.any())
+    f = Fairness()
+    group_value_df = f.get_group_value_fairness(bias_df)
+    print(group_value_df[['group_variable', 'group_value', 'parameter', 'FDR', 'FDR_disparity',
+                          'FDR_ref_group_value', 'TypeI Parity', 'TypeII Parity', 'Impact Parity',
+                          'Statistical Parity', 'Unsupervised Fairness', 'Supervised Fairness']])
+    group_variable_df = f.get_group_variable_fairness(group_value_df)
+    fair = f.get_overall_fairness(group_variable_df)
+    audit_report(configs, fair)
     return
 
 
+def audit_report(configs, fair_results):
+    proj_desc = configs['project_description']
+    print('\n\n\n:::::: REPORT ::::::\n')
+    print('Project Title: ', proj_desc['title'])
+    print('Project Goal: ', proj_desc['goal'])
+    print('Bias Results:', str(fair_results))
+
+    pdf = PDF()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font('Arial', '', 12)
+    pdf.cell(0, 10, 'Project Title: ' + proj_desc['title'], 0, 1)
+    pdf.cell(0, 10, 'Project Goal: ' + proj_desc['goal'], 0, 1)
+    ref_groups = None
+    if 'reference_groups' in configs:
+        ref_groups = str(configs['reference_groups'])
+
+    pdf.cell(0, 10, 'Reference Groups: ' + ref_groups, 0, 1)
+
+    pdf.cell(0, 10, 'Fairness Results: ' + str(fair_results), 0, 1)
+
+    # for i in range(1, 41):
+    #    pdf.cell(0, 10, 'Printing line number ' + str(i), 0, 1)
+    pdf.output('output/aequitas.pdf', 'F')
+
+    return None
 
 def main():
     args = parse_args()
+    print(about)
     if args.format not in {'csv', 'db'}:
         logging.error('Please define input data --format: csv or dsapp (postgres db with DSAPP '
                       'schemas)')
@@ -162,7 +196,7 @@ def main():
         engine = get_engine(configs)
         if args.create_tables:
             create_bias_tables(engine, args.output_schema)
-        run_dsapp(engine, configs)
+        run_db(engine, configs)
 
 
 """
