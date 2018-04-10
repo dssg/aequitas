105358a10cc224a5f622fb2d439620c9f057b4e2
Pedro Saleiro <pedrosaleiro@gmail.com>
2018-04-09 22:32:31 -0500

    polishing report

--- bin/utils/report.py
+++ bin/utils/report.py
@@ -85,10 +85,9 @@ def get_highlevel_report(group_attribute_df):
     for col in group_attribute_df.columns:
         map[col] = col + ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
         # to be able to click on true/false and redirect to the next section
-        if col != 'attribute_name':
-            for attr in attr_list:
-                group_attribute_df.loc[group_attribute_df['attribute_name'] == attr, col] = '[' + group_attribute_df[col][
-                    group_attribute_df['attribute_name'] == attr] + ']' + '(#' + '-'.join(attr.lower().split(' ')) + ')'
+        for attr in attr_list:
+            group_attribute_df.loc[group_attribute_df['attribute_name'] == attr, col] = '[' + group_attribute_df[col][
+                group_attribute_df['attribute_name'] == attr] + ']' + '(#' + '-'.join(attr.lower().split(' ')) + ')'
     group_attribute_df = group_attribute_df.rename(index=str, columns=map)
     highlevel_report = tabulate(group_attribute_df, headers='keys', tablefmt='pipe', showindex='never')
     return highlevel_report
@@ -102,8 +101,7 @@ def get_parity_group_report(group_value_df, attribute, fairness_measures):
     map = {}
     for col in aux_df.columns:
         map[col] = col + ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-        if col in fairness_measures:
-            aux_df[col] = '[' + aux_df[col] + ']' + '(#' + '-'.join(attribute.lower().split(' ')) + '-2)'
+        aux_df[col] = '[' + aux_df[col] + ']' + '(#' + '-'.join(attribute.lower().split(' ')) + '-2)'
     aux_df = aux_df.rename(index=str, columns=map)
     parity_group = tabulate(aux_df,
                             headers='keys',
