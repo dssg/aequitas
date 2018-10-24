78e8f164327d4d9a294f32cd93f827f93530828b
Loren Hinkson <loren.hinkson@gmail.com>
2018-10-24 12:21:21 -0500

    retrieve predefined group

--- src/aequitas/bias.py
+++ src/aequitas/bias.py
@@ -36,6 +36,7 @@ class Bias(object):
             self.fill_divbyzero = 10.00000
         else:
             self.fill_divbyzero = fill_divbyzero
+        self.predefined_groups = None
 
     def get_disparity_min_metric(self, df, key_columns=None, input_group_metrics=None,
                                  fill_divbyzero=None):
@@ -190,6 +191,10 @@ class Bias(object):
         # after division, so if 0/0 we assume 1.0 disparity (they are the same...)
         fill_zeros = {metric: 1.000000 for metric in disparity_metrics}
         # df = df.fillna(value=fill_zeros)
+        
+        # add the latest ref_group as an attribute of the class instance
+        self.predefined_groups = ref_groups_dict
+
         return df
 
     def list_disparities(self, df):
