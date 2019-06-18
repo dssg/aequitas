422128d266eb7ee9fc8e7cddbecf7118692de04e
Pedro Saleiro <pedrosaleiro@gmail.com>
2019-06-18 21:57:10 +0100

    Revert "Signif subset (#70)"
    This reverts commit dd739707001b811d14e6ed617ad79e04abfad502.

--- src/aequitas_cli/aequitas_audit.py
+++ src/aequitas_cli/aequitas_audit.py
@@ -86,11 +86,14 @@ def parse_args():
     return parser.parse_args()
 
 
-def audit(df, configs, preprocessed=False):
+def audit(df, configs, model_id=1, preprocessed=False):
     """
 
     :param df:
+    :param ref_groups_method:
+    :param model_id:
     :param configs:
+    :param report:
     :param preprocessed:
     :return:
     """
@@ -101,50 +104,31 @@ def audit(df, configs, preprocessed=False):
     g = Group()
     print('Welcome to Aequitas-Audit')
     print('Fairness measures requested:', ','.join(configs.fair_measures_requested))
-    groups_model, attr_cols = g.get_crosstabs(df, score_thresholds=configs.score_thresholds,
+    groups_model, attr_cols = g.get_crosstabs(df, score_thresholds=configs.score_thresholds, model_id=model_id,
                                               attr_cols=configs.attr_cols)
     print('audit: df shape from the crosstabs:', groups_model.shape)
     b = Bias()
-
-   # todo move this to the new configs object / the attr_cols now are passed through the configs object...
+    # todo move this to the new configs object / the attr_cols now are passed through the configs object...
     ref_groups_method = configs.ref_groups_method
     if ref_groups_method == 'predefined' and configs.ref_groups:
-        bias_df = b.get_disparity_predefined_groups(groups_model, df, configs.ref_groups,
-                                                    check_significance=configs.check_significance,
-                                                    alpha=configs.alpha,
-                                                    selected_significance=configs.selected_significance,
-                                                    mask_significance=configs.mask_significance)
+        bias_df = b.get_disparity_predefined_groups(groups_model, df, configs.ref_groups)
     elif ref_groups_method == 'majority':
-        bias_df = b.get_disparity_major_group(groups_model, df,
-                                              check_significance=configs.check_significance,
-                                              alpha=configs.alpha,
-                                              selected_significance=configs.selected_significance,
-                                              mask_significance=configs.mask_significance)
+        bias_df = b.get_disparity_major_group(groups_model, df)
     else:
-        bias_df = b.get_disparity_min_metric(df=groups_model, original_df=df,
-                                             check_significance=configs.check_significance,
-                                             alpha=configs.alpha, label_score_ref='fpr',
-                                             selected_significance=configs.selected_significance,
-                                             mask_significance=configs.mask_significance)
-
-
+        bias_df = b.get_disparity_min_metric(groups_model, df)
     print('Any NaN?: ', bias_df.isnull().values.any())
     print('bias_df shape:', bias_df.shape)
 
     aqp = Plot()
 
-    if configs.plot_bias_metrics:
-        if len(configs.plot_bias_metrics) == 1:
-            fig1 = aqp.plot_group_metric(bias_df, group_metric=configs.plot_bias_metrics[0])
-
-        elif len(configs.plot_bias_metrics) > 1:
-            fig1 = aqp.plot_group_metric_all(bias_df, metrics=configs.plot_disparity_attributes)
-
-        if (len(configs.plot_bias_disparities) == 1) and (len(configs.plot_disparity_attributes) == 1):
-            fig2 = aqp.plot_disparity(bias_df, group_metric=configs.plot_bias_disparities[0], attribute_name=configs.plot_disparity_attributes[0])
-
-        elif (len(configs.plot_bias_disparities) > 1) or (len(configs.plot_disparity_attributes) > 1):
-            fig2 = aqp.plot_disparity_all(bias_df, metrics=configs.plot_bias_disparities, attributes=configs.plot_disparity_attributes)
+    if len(configs.plot_bias_metrics) == 1:
+        fig1 = aqp.plot_disparity(bias_df, metrics=configs.plot_bias_metrics)
+    elif len(configs.plot_bias_metrics) > 1:
+        fig1 = aqp.plot_disparity_all(bias_df, metrics=configs.plot_bias_metrics)
+    if len(configs.plot_bias_disparities) == 1:
+        fig2 = aqp.plot_group_metric(bias_df, metrics=configs.plot_bias_disparities)
+    elif len(configs.plot_bias_disparities) > 1:
+        fig2 = aqp.plot_group_metric_all(bias_df, metrics=configs.plot_bias_disparities)
 
     f = Fairness(tau=configs.fairness_threshold)
     print('Fairness Threshold:', configs.fairness_threshold)
@@ -153,16 +137,15 @@ def audit(df, configs, preprocessed=False):
     group_attribute_df = f.get_group_attribute_fairness(group_value_df, fair_measures_requested=configs.fair_measures_requested)
     fair_results = f.get_overall_fairness(group_attribute_df)
 
-    if configs.plot_bias_metrics:
-        if len(configs.plot_bias_metrics) == 1:
-            fig3 = aqp.plot_fairness_group(group_value_df, group_metric=configs.plot_bias_metrics[0])
-        elif len(configs.plot_bias_metrics) > 1:
-            fig3 = aqp.plot_fairness_group_all(group_value_df, metrics=configs.plot_bias_metrics)
+    if len(configs.plot_bias_metrics) == 1:
+        fig3 = aqp.plot_fairness_group(group_value_df, metrics=configs.plot_bias_metrics)
+    elif len(configs.plot_bias_metrics) > 1:
+        fig3 = aqp.plot_fairness_group_all(group_value_df, metrics=configs.plot_bias_metrics)
 
-        if (len(configs.plot_bias_disparities) == 1) and (len(configs.plot_disparity_attributes) == 1):
-            fig4 = aqp.plot_fairness_disparity(group_value_df, group_metric=configs.plot_bias_disparities[0], attribute_name=configs.plot_disparity_attributes[0])
-        elif (len(configs.plot_bias_disparities) > 1) or (len(configs.plot_disparity_attributes) > 1):
-            fig4 = aqp.plot_fairness_disparity_all(group_value_df, metrics=configs.plot_bias_disparities, attributes=configs.plot_disparity_attributes)
+    if len(configs.plot_bias_disparities) == 1:
+        fig4 = aqp.plot_fairness_disparity(group_value_df, metrics=configs.plot_bias_disparities)
+    elif len(configs.plot_bias_metrics) > 1:
+        fig4 = aqp.plot_fairness_disparity_all(group_value_df, metrics=configs.plot_bias_disparities)
 
     print(fair_results)
     report = None
@@ -186,7 +169,7 @@ def run(df, configs, preprocessed=False):
             model_df_list = []
             report_list = []
             for model_id in df.model_id.unique():
-                model_df, model_report = audit(df.loc[df['model_id'] == model_id], configs=configs,
+                model_df, model_report = audit(df.loc[df['model_id'] == model_id], model_id=model_id, configs=configs,
                                                preprocessed=preprocessed)
                 model_df_list.append(model_df)
                 report_list.append(model_report)
