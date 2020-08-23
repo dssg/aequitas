d262716c8e15b1c52d9018db4e9a57f3742c4ee5
Pedro Saleiro <pedro.saleiro@feedzai.com>
2020-08-23 23:12:19 +0100

    Bump version: 0.39.0 → 0.40.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with REQUIREMENTS_PATH.open() as requirements_file:
 
 setup(
     name='aequitas',
-    version='0.39.0',
+    version='0.40.0',
     description="The bias and fairness audit toolkit.",
     long_description=long_description,
     long_description_content_type='text/markdown',
