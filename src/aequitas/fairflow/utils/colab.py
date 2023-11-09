f82e10afbac8d3fc6ad2bed518950d4e899439cb
Sérgio Jesus <sergio.jesus@feedzai.com>
2023-11-09 10:55:31 +0000

    Add helper method to download objects from repo

---
+++ src/aequitas/fairflow/utils/colab.py
@@ -0,0 +1,88 @@
+# Helper file to help download some objects to Google Colab environment.
+import subprocess
+
+from pathlib import Path
+
+from typing import Literal
+
+
+def get_examples(
+    directory: Literal[
+        "configs",
+        "examples/data_repair",
+        "experiment_results",
+    ]
+) -> None:
+    """Downloads the examples from the fairflow repository.
+
+    Parameters
+    ----------
+    directory : Literal["configs", "examples/data_repair", "experiment_results"]
+        The directory to download from the fairflow repository.
+    """
+    # Create directory if it doesn't exist
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    # Check if git repository already exists in folder
+    if not Path(".git").exists():
+        subprocess.run(
+            ["git", "init"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    # Check the remote of the git repository
+    remote = subprocess.run(
+        ["git", "config", "--get", "remote.origin.url"], capture_output=True
+    )
+    if remote.stdout.decode("utf-8") != "https://github.com/dssg/aequitas\n":
+        subprocess.run(
+            [
+                "git",
+                "remote",
+                "add",
+                "-f",
+                "origin",
+                "https://github.com/dssg/aequitas",
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    # Check if sparse checkout is enabled
+    sparse_checkout = subprocess.run(
+        ["git", "config", "--get", "core.sparseCheckout"], capture_output=True
+    )
+
+    first_run = False
+    if sparse_checkout.stdout.decode("utf-8") != "True\n":
+        subprocess.run(
+            ["git", "config", "core.sparseCheckout", "True"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        Path(".git/info/sparse-checkout").touch()
+        first_run = True
+
+    # Check if file in sparse checkout
+    with open(".git/info/sparse-checkout", "r") as f:
+        sparse_checkout_file = f.read()
+    if f"{directory}/**" not in sparse_checkout_file:
+        sparse_checkout_file += f"\n{directory}/**"
+        with open(".git/info/sparse-checkout", "w") as f:
+            f.write(sparse_checkout_file)
+
+    # Pull the files
+    if not first_run:
+        subprocess.run(
+            ["git", "read-tree", "-mu", "HEAD"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    subprocess.run(
+        [
+            "git",
+            "pull",
+            "origin",
+            "fairflow-release",
+        ],  # TODO: Change to main when close to merge
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
