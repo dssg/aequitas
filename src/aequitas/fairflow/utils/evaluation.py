509a09e5a2428904879f0a16f011e3ed31d7183f
Sérgio Jesus <sergio.jesus@feedzai.com>
2023-11-29 17:12:35 +0000

    Improve the code performance and break logic in blocks

--- src/aequitas/fairflow/utils/evaluation.py
+++ src/aequitas/fairflow/utils/evaluation.py
@@ -6,6 +6,10 @@ import pandas as pd
 from ..evaluation import Result
 
 
+# Define constants
+SEED_MULTIPLIER = 1000
+
+
 def _prepare_results(
     results: list[Result],
     dataset_split: Literal["train", "validation", "test"],
@@ -39,12 +43,30 @@ def _prepare_results(
 
 def _calculate_alpha_weighted_metric(
     models: pd.DataFrame,
-    alpha_points: list[float],
+    alpha_points: Union[float, list[float]],
     performance_metric: str,
     fairness_metric: str,
 ) -> pd.DataFrame:
+    """Method to calculate the alpha weighted metric for the given models.
+
+    Parameters
+    ----------
+    models : pd.DataFrame
+        DataFrame with the results for the given dataset split.
+    alpha_points : Union[float, list[float]]
+        Alpha values to use for the bootstrap samples. If float, it is interpreted as a
+        single alpha value. If list, it is interpreted as a list of alpha values.
+    performance_metric : str
+        Name of the performance metric to use.
+    fairness_metric : str
+        Name of the fairness metric to use.
+    """
+
     def calculate_alpha_weighted_score(row, alpha, performance_metric, fairness_metric):
         return row[performance_metric] * alpha + row[fairness_metric] * (1 - alpha)
+
+    if isinstance(alpha_points, float):
+        alpha_points = [alpha_points]
 
     models = models.copy()
     for alpha in alpha_points:
@@ -57,6 +79,67 @@ def _calculate_alpha_weighted_metric(
         )
         models = models.copy()
     return models.copy()
+
+
+def _create_final_results(keys):
+    """Method to create the final results object.
+
+    Parameters
+    ----------
+    keys : list
+        List of keys to use for the final results object.
+    """
+    final_results = {}
+    for key in keys:
+        final_results[key] = {"performance": [], "fairness": [], "alpha_weighted": []}
+    return final_results
+
+
+def _sample_models(models, n_models_to_sample, seed):
+    """Method to sample models from the given DataFrame.
+
+    Parameters
+    ----------
+    models : pd.DataFrame
+        DataFrame with the results for the given dataset split.
+    n_models_to_sample : int
+        Number of models to sample.
+    seed : int
+        Seed for the random number generator and sampling.
+    """
+    np.random.seed(seed)
+    indexes_to_sample = np.random.choice(
+        list(models.index), n_models_to_sample, replace=True
+    )
+    sampled_models = models.loc[indexes_to_sample]
+    return sampled_models
+
+
+def _get_max(models, alpha, best_models):
+    """Method to get the model with the highest alpha weighted metric.
+
+    Parameters
+    ----------
+    models : pd.DataFrame
+        DataFrame with the results for the given dataset split.
+    alpha : float
+        Alpha value to use for the bootstrap samples.
+    best_models : dict
+        Dictionary with the indexes of the best models for each alpha value.
+    """
+    max_index = next(idx for idx in best_models[alpha] if idx in models.index)
+    return models.loc[max_index]
+
+
+def _get_best_models_ordered(models, alpha_points):
+    if isinstance(alpha_points, float):
+        alpha_points = [alpha_points]
+    best_models_ordered = {}
+    for alpha in alpha_points:
+        best_models_ordered[alpha] = (
+            models[f"alpha_{alpha}"].sort_values(ascending=False).index
+        )
+    return best_models_ordered
 
 
 def bootstrap_hyperparameters(
@@ -102,85 +185,70 @@ def bootstrap_hyperparameters(
     if isinstance(alpha_points, list) ^ isinstance(bootstrap_size, list):
         raise ValueError("Only one of alpha_points and bootstrap_size can be a list")
 
+    is_alpha_bootstrap = isinstance(bootstrap_size, float)
+
     # prepare the results object
     models = _prepare_results(results, evaluate_on)
-
-    # Create a list to iterate over the alphas
-    alphas = [alpha_points] if isinstance(alpha_points, float) else alpha_points
-
-    samples = [bootstrap_size] if isinstance(bootstrap_size, float) else bootstrap_size
 
     # Add alpha metrics to the metrics DataFrame
     models = _calculate_alpha_weighted_metric(
         models,
-        alphas,
+        alpha_points,
         performance_metric,
         fairness_metric,
     )
+
+    best_models = _get_best_models_ordered(models, alpha_points)
+
     np.random.seed(seed)
-    sampling_seeds = np.random.choice(n_trials * 1000, n_trials, replace=False)
+    sampling_seeds = np.random.choice(
+        n_trials * SEED_MULTIPLIER, n_trials, replace=False
+    )
 
-    final_results = {}
-    if isinstance(bootstrap_size, float):
-        for alpha in alphas:
-            final_results[alpha] = {
-                "performance": [],
-                "fairness": [],
-                "alpha_weighted": [],
-            }
-    else:
-        for n in samples:
-            final_results[n] = {"performance": [], "fairness": [], "alpha_weighted": []}
-
-    # If we are iterating over alphas:
-    if isinstance(bootstrap_size, float):
+    if is_alpha_bootstrap:
+        final_results = _create_final_results(alpha_points)
         n_models_to_sample = int(round(bootstrap_size * models.shape[0], 0))
+        # Sampling at least one model
         if n_models_to_sample == 0:
             n_models_to_sample = 1
 
+    else:
+        final_results = _create_final_results(bootstrap_size)
+
+    # If we are iterating over alphas:
+    if is_alpha_bootstrap:
         for seed in sampling_seeds:
-            indexes_to_sample = np.random.choice(
-                list(models.index), n_models_to_sample, replace=True
-            )
-            sampled_models = models.loc[indexes_to_sample]
-            for alpha in alphas:
-                selected_model = sampled_models[
-                    sampled_models[f"alpha_{alpha}"]
-                    == sampled_models[f"alpha_{alpha}"].max()
-                ]
+            sampled_models = _sample_models(models, n_models_to_sample, seed)
+            for alpha in alpha_points:
+                selected_model = _get_max(sampled_models, alpha, best_models)
+
                 final_results[alpha]["performance"].append(
-                    selected_model[performance_metric].values[0]
+                    selected_model[performance_metric]
                 )
-                final_results[alpha]["fairness"].append(
-                    selected_model[fairness_metric].values[0]
-                )
+                final_results[alpha]["fairness"].append(selected_model[fairness_metric])
                 final_results[alpha]["alpha_weighted"].append(
-                    selected_model[f"alpha_{alpha}"].values[0]
+                    selected_model[f"alpha_{alpha}"]
                 )
 
     # If we are iterating over bootstraps:
     else:
-        for _, seed in enumerate(sampling_seeds):
+        for seed in sampling_seeds:
+            sampled_models = _sample_models(models, models.shape[0], seed)
             for n in bootstrap_size:
                 n_models_to_sample = int(round(n * models.shape[0], 0))
                 if n_models_to_sample == 0:
                     n_models_to_sample = 1
-                indexes_to_sample = np.random.choice(
-                    list(models.index), n_models_to_sample, replace=True
+                print(sampled_models[:n_models_to_sample].shape[0])
+                selected_model = _get_max(
+                    sampled_models[:n_models_to_sample], alpha_points, best_models
                 )
-                sampled_models = models.loc[indexes_to_sample]
-                selected_model = sampled_models[
-                    sampled_models[f"alpha_{alpha_points}"]
-                    == sampled_models[f"alpha_{alpha_points}"].max()
-                ]
+
                 final_results[n]["performance"].append(
-                    selected_model[performance_metric].values[0]
+                    selected_model[performance_metric]
                 )
-                final_results[n]["fairness"].append(
-                    selected_model[fairness_metric].values[0]
-                )
+                final_results[n]["fairness"].append(selected_model[fairness_metric])
                 final_results[n]["alpha_weighted"].append(
-                    selected_model[f"alpha_{alpha_points}"].values[0]
+                    selected_model[f"alpha_{alpha_points}"]
                 )
 
     return final_results
