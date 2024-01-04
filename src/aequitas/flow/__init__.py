4305ae9fe482cd42a8c9d1575e0dacdbfd4902a2
Sérgio Jesus <sergio.jesus@feedzai.com>
2024-01-04 15:10:38 +0000

    Remove debug code

--- src/aequitas/flow/__init__.py
+++ src/aequitas/flow/__init__.py
@@ -2,12 +2,3 @@ from ._version import __version__
 from .experiment import Experiment
 
 __all__ = ["__version__", "Experiment"]
-
-
-class test:
-    def __init__(self):
-        pass
-
-    def test(self):
-        """Hey"""
-        print("test")
