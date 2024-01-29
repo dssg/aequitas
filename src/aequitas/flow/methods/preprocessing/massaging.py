109311dc74343b1f28292740a900c8d3a4022305
Inês Silva <inesoliveiraesilva@gmail.com>
2024-01-29 10:51:29 +0000

    Added massaging method

---
+++ src/aequitas/flow/methods/preprocessing/massaging.py
@@ -0,0 +1,64 @@
+from typing import Optional
+
+import pandas as pd
+import math
+from sklearn.naive_bayes import GaussianNB
+
+from ...utils import create_logger
+from .preprocessing import PreProcessing
+
+
+class Massaging(PreProcessing):
+    def __init__(self):
+        self.logger = create_logger("methods.preprocessing.Massaging")
+        self.logger.info("Instantiating a Massaging preprocessing method.")
+
+    def _rank(
+        self, X: pd.DataFrame, y: pd.Series, s: Optional[pd.Series]
+    ) -> tuple[list, list]:
+        features = pd.concat([X, s], axis=1)
+        features = pd.get_dummies(features)
+        R = GaussianNB().fit(features, y)
+        scores = pd.Series(R.predict_proba(features)[:, 1], index=X.index)
+
+        pr = []
+        dem = []
+
+        for g in s.unique():
+            prevalence = y[s == g].mean()
+            if prevalence < y.mean():
+                pr += list(X.loc[(s == g) & (y == 0)].index)
+            elif prevalence > y.mean():
+                dem += list(X.loc[(s == g) & (y == 1)].index)
+
+        pr = scores.loc[pr].sort_values(ascending=False).index
+        dem = scores.loc[dem].sort_values(ascending=True).index
+
+        return pr, dem
+
+    def fit(self, X: pd.DataFrame, y: pd.Series, s: Optional[pd.Series]) -> None:
+        self.logger.info("Fitting Massaging preprocessing method.")
+        self.pr, self.dem = self._rank(X, y, s)
+
+        g_pr = s.loc[self.pr].unique()
+        g_dem = s.loc[self.dem].unique()
+
+        d_b = y.loc[s.isin(g_pr)].mean()
+        d_w = y.loc[s.isin(g_dem)].mean()
+        d = d_w - d_b
+
+        self.m = math.ceil(
+            (d * y.loc[s.isin(g_pr)].shape[0] * y.loc[s.isin(g_dem)].shape[0])
+            / y.shape[0]
+        )
+        self.logger.info("Massaging preprocessing method fitted.")
+
+    def transform(
+        self, X: pd.DataFrame, y: pd.Series, s: Optional[pd.Series] = None
+    ) -> tuple[pd.DataFrame, pd.Series, pd.Series]:
+        self.logger.info("Transforming data with Massaging preprocessing method.")
+        y_corrected = y.copy()
+        y_corrected.loc[self.pr[: self.m]] = 1
+        y_corrected.loc[self.dem[: self.m]] = 0
+        self.logger.info("Data transformed with Massaging preprocessing method.")
+        return X, y_corrected, s
